83f72545e41b4f15dcf7870b4f30a24477bced49
burhaan <batargett@gmail.com>
2019-04-01 11:49:30 +1100

    WIP

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace spike_azure_active_directory.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
--- Startup.cs
+++ Startup.cs
@@ -1,7 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -10,6 +15,10 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication.AzureAD.UI;
+using spike_azure_active_directory.Extensions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
 
 namespace spike_azure_active_directory
 {
@@ -25,7 +34,33 @@ namespace spike_azure_active_directory
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = OpenIdConnectDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddAzureAdBearer(options =>
+            {
+                Configuration.Bind("AzureAd", options);
+                AzureAdOptions.Settings = options;
+            });
+
+
+            // services.Configure<OpenIdConnectOptions>(AzureADDefaults.OpenIdScheme, options =>
+            // {
+            //     options.Authority = options.Authority + "/v2.0/";
+            //     options.TokenValidationParameters.ValidateIssuer = false;
+            // });
+
+            // services.AddMvc(options =>
+            // {
+            //     var policy = new AuthorizationPolicyBuilder()
+            //         .RequireAuthenticatedUser()
+            //         .Build();
+            //     options.Filters.Add(new AuthorizeFilter(policy));
+            // }).SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().AddSessionStateTempDataProvider();
+            services.AddSession();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +68,7 @@ namespace spike_azure_active_directory
         {
             if (env.IsDevelopment())
             {
+
                 app.UseDeveloperExceptionPage();
             }
             else
@@ -40,7 +76,8 @@ namespace spike_azure_active_directory
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseSession();
+            app.UseAuthentication();
             app.UseHttpsRedirection();
             app.UseMvc();
         }
